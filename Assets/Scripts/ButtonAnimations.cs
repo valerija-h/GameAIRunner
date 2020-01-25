0a1d572de90d2f4a7f91f25d692367b260a22df8
Deborah <deborah.vella.17@um.edu.mt>
2020-01-25 18:59:01 +0100

    Main Menu, Started Tutorial

---
+++ Assets/Scripts/ButtonAnimations.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ButtonAnimations : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Animator animator;
+        private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        animator.enabled = false;
+    }
+    private bool mouse_over = false;
+
+        void Update()
+        {
+            if (mouse_over)
+            {
+            animator.enabled = true;
+        }
+        else
+        {
+            animator.enabled = false;
+        }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            mouse_over = true;
+            Debug.Log("Mouse enter");
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            mouse_over = false;
+        
+        }
+    }
+    
---
+++ Assets/Scripts/ManageScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ManageScene : MonoBehaviour
+{
+    int currentScene;
+    int nextScene;
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        nextScene = currentScene + 1;
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+    
+    public void goToTutorial()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+}
